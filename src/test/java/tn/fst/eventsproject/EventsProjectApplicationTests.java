3423898a6f3fcf28e5bf0b8f2764a013ea949bf6
souleimen284 <souleimen.benhmida@gmail.com>
2025-12-08 15:19:55 +0100

    Update EventsProjectApplicationTests.java

--- src/test/java/tn/fst/eventsproject/EventsProjectApplicationTests.java
+++ src/test/java/tn/fst/eventsproject/EventsProjectApplicationTests.java
@@ -3,11 +3,10 @@ package tn.fst.eventsproject;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
+@ActiveProfiles("test")
 class EventsProjectApplicationTests {
-
     @Test
-    void contextLoads() {
-    }
-
+    void contextLoads() {}
 }
